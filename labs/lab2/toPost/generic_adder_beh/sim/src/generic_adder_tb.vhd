c5bd8b098dc226fb2dec51efbe6ec53fade97339
Rachel DuBois <rjd5748@rit.edu>
2023-09-18 17:44:23 -0400

    update  to 4

--- labs/lab2/toPost/generic_adder_beh/sim/src/generic_adder_tb.vhd
+++ labs/lab2/toPost/generic_adder_beh/sim/src/generic_adder_tb.vhd
@@ -25,11 +25,11 @@ component generic_adder_beh is
   );
 end component generic_adder_beh;
 
-constant NUM_BITS          : integer := 2;
-constant SEQUENTIAL_FLAG   : boolean := false;     -- false : concurrent stimuli, true: sequential stimuli
+constant NUM_BITS          : integer := 4;
+constant SEQUENTIAL_FLAG   : boolean := true;     -- false : concurrent stimuli, true: sequential stimuli
 signal a                   : std_logic_vector(NUM_BITS - 1 downto 0) := (others => '0');
 signal b                   : std_logic_vector(NUM_BITS - 1 downto 0) := (others => '0');
-signal cin                 : std_logic := '0';
+signal cin                 : std_logic := '1';
 signal sum                 : std_logic_vector(NUM_BITS - 1 downto 0);
 signal cout                : std_logic;
 signal cin_guard           : std_logic_vector(NUM_BITS - 2 downto 0) := (others => '0');
@@ -48,20 +48,42 @@ uut: generic_adder_beh
     cout    => cout
   );
 
+--SEQUENTIAL without cin  
+--sequential_stimuli: if SEQUENTIAL_FLAG generate
+--  sequential_tb : process 
+--    begin
+--      report "****************** sequential testbench start ****************";
+--      wait for 10 ns;   -- let all the initial conditions trickle through
+--      for i in 0 to ((2 ** NUM_BITS) - 1) loop
+--        a <= std_logic_vector(unsigned(a) + 1 );
+--        for j in 0 to ((2 ** NUM_BITS) - 1)  loop
+--          b <= std_logic_vector(unsigned(b) + 1 );
+--          wait for 10 ns;
+--        end loop;
+--      end loop;
+--      report "****************** sequential testbench stop ****************";
+--      wait;
+--  end process; 
+--end generate sequential_stimuli;
+
+--WITH cin
 sequential_stimuli: if SEQUENTIAL_FLAG generate
   sequential_tb : process 
     begin
       report "****************** sequential testbench start ****************";
       wait for 10 ns;   -- let all the initial conditions trickle through
-      for i in 0 to ((2 ** NUM_BITS) - 1) loop
-        a <= std_logic_vector(unsigned(a) + 1 );
-        for j in 0 to ((2 ** NUM_BITS) - 1)  loop
-          b <= std_logic_vector(unsigned(b) + 1 );
-          wait for 10 ns;
+      for k in 0 to 1 loop
+        cin <= not cin;
+        for i in 0 to ((2 ** NUM_BITS) - 1) loop
+          a <= std_logic_vector(unsigned(a) + 1 );
+          for j in 0 to ((2 ** NUM_BITS) - 1)  loop
+            b <= std_logic_vector(unsigned(b) + 1 );
+            wait for 10 ns;
+            end loop;
+          end loop;
         end loop;
-      end loop;
-      report "****************** sequential testbench stop ****************";
-      wait;
+        report "****************** sequential testbench stop ****************";
+        wait;
   end process; 
 end generate sequential_stimuli;
 
