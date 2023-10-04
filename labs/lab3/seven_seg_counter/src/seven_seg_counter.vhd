abadadfcce2afb782100393d616c4f7fdec4280b
Rachel DuBois <rachel@duboishouse.com>
2023-10-04 17:24:33 -0400

    Tidy ciode

--- labs/lab3/seven_seg_counter/src/seven_seg_counter.vhd
+++ labs/lab3/seven_seg_counter/src/seven_seg_counter.vhd
@@ -8,11 +8,9 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 entity seven_seg_counter is
-	Port ( 	clk 	    : in  STD_LOGIC; --Input clock
-			--bcd			: in  STD_LOGIC_VECTOR(3 downto 0) := "0001";
+	Port ( 	clk 	        : in  STD_LOGIC; --Input clock
 			reset			: in  STD_LOGIC;
-			--
-			seven_seg_outp	    : out STD_LOGIC_VECTOR(6 downto 0)  --7-seg display outputs (g to a) for 7seg display
+			seven_seg_outp  : out STD_LOGIC_VECTOR(6 downto 0)  --7-seg display outputs (g to a) for 7seg display
 			); 
 end seven_seg_counter;
 
@@ -27,8 +25,8 @@ component seven_seg is
 	port (
 		clk                : in  std_logic;
 		reset              : in std_logic;
-	  	bcd           : in  std_logic_vector(3 downto 0);
-	  	seven_seg_out          : out std_logic_vector(6 downto 0)
+	  	bcd                : in  std_logic_vector(3 downto 0);
+	  	seven_seg_out      : out std_logic_vector(6 downto 0)
 	);  
   end component; 
 
@@ -48,12 +46,10 @@ component seven_seg is
   component generic_adder_beh is
 	port (
 	  a             : in  std_logic_vector(3 downto 0); 
-	  b           : in  std_logic_vector(3 downto 0);
-	  cin : in std_logic;
-
-	  sum : out std_logic_vector(3 downto 0);
+	  b             : in  std_logic_vector(3 downto 0);
+	  cin           : in std_logic;
+	  sum           : out std_logic_vector(3 downto 0);
 	  cout          : out std_logic
-
 	);  
   end component; 
 
@@ -65,7 +61,6 @@ gen_add: generic_adder_beh
 	  a => sum_sig,
 	  b => "0001",
 	  cin => '0',
-	  --
 	  sum => sum,
 	  cout => open
 	);
@@ -76,7 +71,6 @@ counter: generic_counter
 	port map(
 	  clk => clk,
 	  reset => reset,
-	  --
 	  output => enable
 	);
 bdc_convert: seven_seg
@@ -84,19 +78,18 @@ port map(
 	clk => clk,
 	reset => reset,
  	bcd => sum_sig,
-  	--
   	seven_seg_out => seven_seg_outp
   
 );
 convert : process(clk, reset)
 begin
-if (reset = '1') then
-	 sum_sig <="1111";
-elsif (clk'event and clk = '1') then
-	if (enable = '1') then
-		sum_sig <= sum;
+	if (reset = '1') then
+	 	sum_sig <="1111";
+	elsif (clk'event and clk = '1') then
+		if (enable = '1') then
+			sum_sig <= sum;
+		end if;
 	end if;
-end if;
-  END PROCESS convert;
+END PROCESS convert;
 
 end beh;
