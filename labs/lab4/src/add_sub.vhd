26379bcc9d2fe7ce2fe8b831469c77e0153b2e70
Rachel DuBois <rachel@duboishouse.com>
2023-10-16 12:36:57 -0400

    entity

--- labs/lab4/src/add_sub.vhd
+++ labs/lab4/src/add_sub.vhd
@@ -3,3 +3,21 @@
 -- Add and subtract 
 -------------------------------------------------------------------------------
 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeroc_std.all;
+
+entity add_sub is
+    port(
+        clk : in std_logic;
+        reset : in std_logic;
+        a : in std_logic_vector(2 downto 0);
+        b : in std_logic_vector(2 downto 0);
+        add_btn : in std_logic; 
+        sub_btn : in std_logic; 
+        a_bcd : out std_logic_vector(6 downto 0);
+        b_bcd : out std_logic_vector(6 downto 0);
+        result_bcd : out std_logic_vector(6 downto 0)
+    );
+end entity add_sub;
+
